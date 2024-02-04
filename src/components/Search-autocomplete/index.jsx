3e2f80f613bc8764fddf441cafae88aed2eb4a73
kumarvivekpandey <35356580+kumarvivekpandey@users.noreply.github.com>
2024-02-04 17:47:26 +0000

    loading

--- src/components/Search-autocomplete/index.jsx
+++ src/components/Search-autocomplete/index.jsx
@@ -1,7 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function SearchAutocomplete() {
+  const [loading, setLoading] = useState(false);
+
   const [user, setUser] = useState("");
+
+  async function fetchuser() {
+    try {
+      setLoading(true);
+      await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await response.json();
+      setUser(data);
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchuser(url);
+  }, []);
   return (
     <div className="search-autocomplete">
       <input
@@ -9,9 +27,8 @@ export default function SearchAutocomplete() {
         name="searchcomplete"
         placeholder="enter text"
         value={user}
-       onChange={(e) => setUser(e.target.value)}
+        // onChange={(e) => setUser(e.target.value)}
       />
-      <p> {user}</p>
     </div>
   );
 }
